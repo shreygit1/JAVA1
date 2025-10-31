bc2091ce8b778da85aefc7496a8597d30bb10c5a
shreygit1 <133968515+shreygit1@users.noreply.github.com>
2025-10-31 12:48:53 +0530

    Add files via upload

---
+++ drivinglicense.java
@@ -0,0 +1,16 @@
+public class drivinglicense {
+    
+
+    public static void main(String[] args) {
+        int age = 15;
+        
+
+        // Using if-else if-else statements
+        if (age >= 17 && age <= 79) {
+            System.out.println(age + " is eligible for license");
+        } else {
+            System.out.println(age + " is not eligible for license");
+        } 
+    
+}
+}
